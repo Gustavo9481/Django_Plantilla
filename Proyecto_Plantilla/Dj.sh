17f13d7e5ccd68ebee2c07a3394d3b04a2c7aefe
Gustavo9481 <guscode.apps@gmail.com>
2023-10-31 23:56:43 -0500

    Gestor de proyecto en Bash

---
+++ Proyecto_Plantilla/Dj.sh
@@ -0,0 +1,69 @@
+#! /bin/zsh
+
+# GESTOR DE PROYECTOS DJANGO
+# Ubicar archivo en la carpeta de Proyecto General (core)
+
+# -------------------------------------------------------------------- Funciones
+# funciones para manage.py que soliciten parámetros
+
+# ---------------------------------------------------------------------- project
+function project(){
+    # Crea el proyecto solicitando el nombre del mismo
+    echo "Nombre del Proyecto: ${yellow}[ usar prefijo Proyecto_ ]${blanco}"
+    read nombre_project
+    eval "django-admin startproject ${nombre_project}"
+    echo "Creado el proyecto ${turquoise}${nombre_project}${blanco}"
+}
+
+
+# -------------------------------------------------------------------------- app
+function app(){
+    # Crea aplicaciones solicitando el nombre
+    echo "Nombre de nueva Aplicación: ${yellow}[ usar el prefijo App_ ]${blanco}"
+    read nombre_app
+    eval "python manage.py startapp ${nombre_app}"
+    echo "Creada la aplicación ${turquoise}${nombre_app}${blanco}"
+}
+
+
+# ----------------------------------------------------------- Colores para texto
+blanco="\033[0m\e[0m"
+green="\e[0;32m\033[1m"
+red="\e[0;31m\033[1m"
+blue="\e[0;34m\033[1m"
+yellow="\e[0;33m\033[1m"
+turquoise="\e[0;36m\033[1m"
+purple="\e[0;35m\033[1m"
+grey="\e[0;30m\033[1m"
+
+
+# ------------------------------------- instrucciones para el Gestor de Proyecto
+# Diccionario contenedor de instrucciones para el gestor
+# dict[instruccion]=tarea
+
+declare -A dict
+dict[run]="python manage.py runserver"
+dict[proyecto]=project
+dict[migraciones]="python manage.py makemigrations"
+dict[migrar]="python manage.py migrate"
+dict[aplicacion]=app
+
+
+
+
+
+# --------------------------------------------------------- Ejecución del Script
+# Presentación del Gestor al usuario y solicitud de la instrucción
+
+echo -e ${turquoise} GUScode ${blanco} Gestor de proyectos Django 
+echo -e Instrucción ${turquoise}󰒊${blanco}  
+read tarea
+
+
+if [[ ${dict[$tarea]} ]]; then
+    echo "Ejecutando =>\n"
+    eval ${dict[$tarea]}
+else
+    echo "La instrucción no es correcta"
+fi
+
